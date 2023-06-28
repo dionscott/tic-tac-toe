8e2e2496b1a7b1abe1a3a3a7067238466798fcaf
Dion Scott <dionanthonyscott@gmail.com>
2023-06-28 11:16:55 -0500

    Add function to swap turns and check if game is won

--- script.rb
+++ script.rb
@@ -7,14 +7,19 @@
 
 # shows the board and keeps track of players marking
 class Board
-  # Initialize by creating a board
+  # Initialize by creating a board and setting game_won to false
   def initialize
     create_board
+    @game_won = false
+  end
+
+  def game_won
+    @game_won
   end
 
   # board creates a new game
   def create_board
-    # create three rows with arrays to mark
+    # create an array with numbers 1 to 9
     @board = Array(1..9)
   end
 
@@ -25,24 +30,20 @@ class Board
     p @board[6..8]
   end
 
-  # change the board based on player
+  # mark the board based on player mark
   def mark_board(position, player)
-    # find the position or return nil
     index = @board.index(position)
-    # check if the position has been taken
+    @board[index] = player.mark
+    show_board
+  end
+
+  #check if the position has been taken by another player
+  def valid_position?(position)
+    index = @board.index(position)
     if index
-      @board[index] = player.mark
-      if won?
-        p "Congratulations Player #{player.mark} has won!"
-        show_board
-      else
-        player.change_turn
-        p "Next Player"
-        show_board
-      end
+      true
     else
-      p "This spot is not avaliable. Choose again."
-      show_board
+      false
     end
   end
 
@@ -52,13 +53,11 @@ class Board
     win_conditions = win_condition
     # if any of these all match each each other
     if win_conditions.any? { |condition| condition.uniq.size <= 1 }
-      true
-    else 
-      false
+      @game_won = true
     end
   end
 
-  # This might be able to be a constant
+  # All avaliable win conditions
   def win_condition
     win_conditions = [] 
     win_conditions << [@board[0], @board[1], @board[2]]
@@ -80,15 +79,21 @@ class Player
     if x_or_o == "X"
       @mark = "X"
       @turn = true
+      @name = "Player 1"
     else
       @mark = "O"
       @turn = false
+      @name = "Player 2"
     end
   end
 
   # Shows the mark of the player
   def mark
     @mark
+  end
+
+  def name
+    @name
   end
 
   # Returns true or false depending on whose turn
@@ -98,7 +103,7 @@ class Player
 
   # Change the turn
   def change_turn
-    !@turn
+    @turn = !@turn
   end
 end
 
@@ -109,17 +114,39 @@ def play_game
   player_1 = Player.new("X")
   player_2 = Player.new("O")
 
+  # swap player 1 and player 2 turns
+  def swap_turns(p1 = player_1, p2 = player_2)
+    p1.turn = p1.change_turn
+    p2.turn = p2.change_turn
+  end
+  # create game function
+  until board.game_won
+    p "Enter the position you would like to play."
+    position = gets.chomp.to_i
+
+    if board.valid_position?(position)
+      if player_1.turn
+        p player_1.turn
+        board.mark_board(position, player_1)
+      else
+        board.mark_board(position, player_2)
+      end
+        # change player turn
+        # start again from player 2
+        # check until game is won
+        player_1.change_turn
+        player_2.change_turn
+        board.won?
+    else
+      p "Invalid position. Try again"
+    end
+  end
+  # player 1 chooses a spot
+  # check if that spot is avaliable
+  # if it is place the mark
+  # if it isn't restart and try to place mark again
+  p "The player won the game"
   # continue game until won
 end
 
-board = Board.new
-  board.show_board
-  player_1 = Player.new("X")
-  player_2 = Player.new("O")
-  board.mark_board(1, player_1)
-  board.mark_board(5, player_1)
-  board.mark_board(6, player_1)
-  board.mark_board(6, player_1)
-  board.mark_board(1, player_1)
-  board.mark_board(4, player_2)
-  p board.won?
+play_game
